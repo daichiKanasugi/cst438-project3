72ad4a49d2b42d785f27a593c4d914d1922b1943
daichiKanasugi <42822207+daichiKanasugi@users.noreply.github.com>
2020-04-14 17:21:32 -0700

    mongoDB

--- project3/backend/server.js
+++ project3/backend/server.js
@@ -5,12 +5,13 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+//const uri = process.env.ATLAS_URI;
+
+mongoose.connect('mongodb+srv://daichi:112294@cluster0-0znom.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true, useCreateIndex: true  }
 );
 
 // <--Connects to the Mongo DB!! -->
