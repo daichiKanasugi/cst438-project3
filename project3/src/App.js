0c69603c97ff9daef75585c265215200396c92a0
FaustosWork <jofausto@csumb.edu>
2020-04-24 20:47:18 -0700

    adding updates to personal branch

--- project3/src/App.js
+++ project3/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Create from './components/create.component.js'
+import Create from './components/home.component.js'
 
 function App() {
   return (
